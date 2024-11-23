f70b9c929302e8202378ae17353b1f3af19f296f
maxim12313 <maximkim16@gmail.com>
2024-11-23 13:08:30 -0500

    auto add: test[11-23].cpp

---
+++ solutions/test[11-23].cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    cout << "Hello world\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
