5a0fb010f28f4eee774457b2cedd12b83903c9a5
maxim12313 <maximkim16@gmail.com>
2024-11-24 03:37:12 -0500

    now handle spaces and / by converting to -

--- put.sh
+++ put.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 date=$(date +%m-%d)
+
+# name in format name-m-d
 name="$2-($date).cpp"
-mkdir solutions
-cp $1.cpp solutions/$name
-git add solutions/$name
+
+# get replace all bad characters with -
+options=" /"
+name=$(echo "$name" | sed "s:[$options]:-:g")
+
+# create temp folder for github structure
+mkdir -p solutions
+
+# temporarily move to folder
+cp $1.cpp "solutions/$name"
+
+# add and push from folder
+git add "solutions/$name"
 git commit -m "auto add: $name"
+
+# cleanup
 rm -rf solutions
