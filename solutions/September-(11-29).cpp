a6db25d22289854ad27326fb2f2d194e95f7056b
maxim12313 <maximk@umich.edu>
2024-11-29 13:57:20 -0800

    auto add: September-(11-29).cpp

---
+++ solutions/September-(11-29).cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <pthread.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int count = 0;
+    for (int i = 1; i <= 12; i++) {
+        string s;
+        cin >> s;
+        if (size(s) == i)
+            count++;
+    }
+    cout << count << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+}
