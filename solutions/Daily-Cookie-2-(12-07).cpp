3bfd02f01e13bff8d652356470b15152d4e31e7d
maxim12313 <maximk@umich.edu>
2024-12-07 14:01:36 -0500

    auto add: Daily-Cookie-2-(12-07).cpp

---
+++ solutions/Daily-Cookie-2-(12-07).cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cassert>
+#include <cfenv>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <pthread.h>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n, d;
+    cin >> n >> d;
+    string s;
+    cin >> s;
+    for (int i = n - 1; i >= 0 && d > 0; i--)
+        if (s[i] == '@') {
+            s[i] = '.';
+            d--;
+        }
+    cout << s << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+}
